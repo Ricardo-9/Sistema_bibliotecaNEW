92b68e8a1f2cceb96febc6319fb37b7e9b8610f2
Ricardo-9 <ricardorochaalves550@gmail.com>
2025-05-23 20:58:32 -0300

    env

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://wozdxvknegdophxocbhw.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvemR4dmtuZWdkb3BoeG9jYmh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTE4NDEsImV4cCI6MjA2MzIyNzg0MX0.EQrBRda7WMT3Uwi7eFjYWHEYxQ3h_SWASrUCVclxuXU'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
